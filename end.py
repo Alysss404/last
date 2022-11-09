2591fb61b2b038a9d49850bacd0042b9113a4063
Alysss404 <116890012+Alysss404@users.noreply.github.com>
2022-11-09 14:26:05 +0800

    Update end.py

--- end.py
+++ end.py
@@ -1,4 +1,6 @@
 import streamlit as st
 import pandas as pd
+st.title("生态驾驶评分系统")
 sample_df = pd.read_csv("sample1.csv")
 st.write(sample_df)
+vid_option = st.sidebar.selectbox('choose the vehicle_id:', sample_df['vehid'])
