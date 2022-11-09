af9cede21285e978f3b51ef0ab52ef5310390e2c
Alysss404 <116890012+Alysss404@users.noreply.github.com>
2022-11-09 14:43:12 +0800

    Update end.py

--- end.py
+++ end.py
@@ -4,5 +4,5 @@ st.title("生态驾驶评分系统")
 sample_df = pd.read_csv("sample1.csv")
 st.write(sample_df)
 vid = sample_df['vehid']
-vid.drop_duplicates(subset=['vehid'], keep='first', inplace=True)
+vid.drop_duplicates(keep='first', inplace=True)
 vid_option = st.sidebar.selectbox('choose the vehicle_id:', vid)
