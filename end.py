6321857c87f843384a278d4d5ea778f202606f85
Alysss404 <116890012+Alysss404@users.noreply.github.com>
2022-11-09 14:46:05 +0800

    Update end.py

--- end.py
+++ end.py
@@ -6,3 +6,7 @@ st.write(sample_df)
 vid = sample_df['vehid']
 vid.drop_duplicates(keep='first', inplace=True)
 vid_option = st.sidebar.selectbox('choose the vehicle_id:', vid)
+
+oid = sample_df['orderid', 'orderid' = {}].formart(vid_option)
+oid.drop_duplicates(keep='first', inplace=True)
+oid_option = st.sidebar.selectbox('choose the order_id:', oid)
