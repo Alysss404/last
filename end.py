26d533c92fa6bfb096e261d43cac0a00a05b8176
Alysss404 <116890012+Alysss404@users.noreply.github.com>
2022-11-09 15:25:09 +0800

    Update end.py

--- end.py
+++ end.py
@@ -14,6 +14,24 @@ oid = sample_df['orderid']
 oid.drop_duplicates(keep='first', inplace=True)
 oid_option = st.sidebar.selectbox('choose the order_id:', oid)
 
-option_df = sample_df['vehid' == '{}','orderid' == '{}'].format(vid_option,oid_option)
+# option_df = sample_df['vehid' == '{}','orderid' == '{}'].format(vid_option,oid_option)
+option_df = sample_df['vehid' == 2,'orderid' == 4551]
 st.write(option_df)
 st.map(option_df)
+
+st.header("评分")
+st.text("该车辆对应订单驾驶行为分析及其分数：")
+score_df = pd.read_csv("score3.csv")
+score = score_df['orderid' == 4551]
+st.write(score_df)
+expander = st.expander("添加新的数据")
+with expander:
+
+    uploaded_files = st.file_uploader("请选择一个CSV文件", accept_multiple_files=True)
+    uploaddf = pd.DataFrame()
+    for uploaded_file in uploaded_files:
+        uploaddf = pd.read_csv(uploaded_file)
+
+        st.write(uploaddf)
+    if st.button('上传到数据库'):
+        st.success("上传成功!")
